97d3b66026de13de2a7bafe1d9823dce8d7d6713
Narhzih <omosekejiolawale@gmail.com>
2021-09-20 15:57:03 +0100

    Created parts of UI

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,97 @@
 <template>
   <div id="app">
+    <div class="app-banner">
+      <div class="container">
+        <div class="row d-flex align-items-center">
+          <div class="col-md-6 text-start">
+            <h1 class="text-start app-banner__header">
+              Growth partner for
+              small businesses
+            </h1>
+            <p class="app-banner__sub">Enjoy asset loans, automated business management, and business reports summarized in plain English.</p>
+            <a href="#" class="app-banner__button">Get Early Access Now</a>
+          </div>
 
+          <div class="col-md-6">
+            <img src="@/assets/images/figure_1.png">
+          </div>
+        </div>
+        <img src="@/assets/images/chat_icon.svg" class="app-banner__chat-icon" alt="bottom-icon">
+      </div>
+    </div>
+    <div class="app__calculator">
+      <calculator></calculator>
+    </div>
   </div>
 </template>
 
 <script>
 
 
+import Calculator from "./components/Calculator";
 export default {
   name: 'App',
   components: {
+    Calculator
 
   }
 }
 </script>
 
-<style>
+<style >
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+
+.app-banner__chat-icon {
+  position: absolute;
+  left: 95%;
+  top: 70%;
+  transform: translateX(-95%);
+}
+
+.app-banner {
+  position: relative;
+  padding: 5rem 20rem;
+  min-height: 100vh;
+  width: 100%;
+  background: transparent linear-gradient(180deg, #EEE8FE 0%, #4B397E00 100%) 0% 0% no-repeat padding-box;
+
+}
+.app-banner__header {
+  color: #021C34;
+  font-weight: 800;
+  font-size: 70px;
+  font-family: 'Mulish', sans-serif;
+  letter-spacing: -2.1px;
+}
+
+.app-banner__sub {
+  display: inline-block;
+  margin-top: 1rem;
+}
+.app-banner__button {
+  background: #9672FB 0% 0% no-repeat padding-box;
+  border-radius: 5px;
+  opacity: 1;
+  padding: 1rem;
+  text-decoration: none;
+  color: #ffffff;
+  display: inline-block;
+  margin-top: 1rem;
+}
+
+.app-banner__button:hover {
+  color: #ffffff;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <h3 class="text-center c-head">Cash flow is king</h3>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card card--input text-start">
+          <h4>Current cashflow</h4>
+          <h2>How much do you make?</h2>
+          <form action="#" >
+            <input type="number" placeholder="Enter amount" class="form-control" v-model="amount">
+          </form>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card card--output text-start">
+          <h4>With pennee</h4>
+          <h2>How much you'll make</h2>
+          <h1 class="c-result">{{calculated_amount}}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data() {
+    return {
+      amount: 0,
+      calculated_amount: 0.00
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .c-head {
+    font: normal normal 800 50px/74px Mulish;
+    letter-spacing: 0px;
+    color: #021C34;
+    opacity: 1;
+    margin-bottom: 5rem;
+  }
+  .card {
+    padding: 3rem;
+    min-height: 30rem !important;
+  }
+  .card--input {
+    background-color: #FEF5F2;
+  }
+  .card--output {
+    background-color: #EFEAFF;
+  }
+  .card > h4 {
+    font: normal normal bold 26px/35px Mulish;
+    letter-spacing: 0px;
+    color: #021C34;
+    opacity: 1;
+  }
+  .card > h2 {
+    font: normal normal 800 43px/55px Mulish;
+    letter-spacing: 0px;
+    color: #021C34;
+    opacity: 1;
+
+  }
+
+  .card > form {
+    position: absolute;
+    width: 85%;
+    left: 50%;
+    top: 80%;
+    transform: translateX(-50%);
+  }
+  .c-result {
+    position:absolute;
+    top: 75%;
+    text-align: left;
+    font: normal normal 800 70px/90px Mulish;
+    letter-spacing: 0px;
+    color: #021C34;
+    opacity: 1;
+
+  }
+</style>
